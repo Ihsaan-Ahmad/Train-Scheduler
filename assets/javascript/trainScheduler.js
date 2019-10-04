873d84c576e96c8c1434567a7f822f50acacbbfc
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-10-03 22:33:03 -0500

    fixed broken code

--- assets/javascript/trainScheduler.js
+++ assets/javascript/trainScheduler.js
@@ -1,98 +1,133 @@
-var config = {
-  apiKey: "AIzaSyDj8NT1udplkHNBZRZVNOLFxzPnHr9VZP0",
-  authDomain: "train-scheduler-e13bf.firebaseapp.com",
-  databaseURL: "https://train-scheduler-e13bf.firebaseio.com",
-  projectId: "train-scheduler-e13bf",
-  storageBucket: "train-scheduler-e13bf.appspot.com",
-  messagingSenderId: "516547437802",
-  appId: "1:516547437802:web:c3474bc3d8b73c73faa393"
-};
-
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-$("#newTrainBtn").on("click", function(event) {
-  event.preventDefault();
-  var trainName = $("#trainNameInput")
-    .val()
-    .trim();
-  var destination = $("#destInput")
-    .val()
-    .trim();
-  var firstTrain = $("#firstTrainInput")
-    .val()
-    .trim();
-  var frequency = $("#freqInput")
-    .val()
-    .trim();
-
-  var newTrain = {
-    name: trainName,
-    dest: destination,
-    first: firstTrain,
-    freq: frequency
+$(document).ready(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDj8NT1udplkHNBZRZVNOLFxzPnHr9VZP0",
+    authDomain: "train-scheduler-e13bf.firebaseapp.com",
+    databaseURL: "https://train-scheduler-e13bf.firebaseio.com",
+    projectId: "train-scheduler-e13bf",
+    storageBucket: "train-scheduler-e13bf.appspot.com",
+    messagingSenderId: "516547437802",
+    appId: "1:516547437802:web:c3474bc3d8b73c73faa393"
   };
 
-  database.ref().push(newTrain);
+  firebase.initializeApp(config);
 
-  console.log(newTrain.name);
-  console.log(newTrain.dest);
-  console.log(newTrain.first);
-  console.log(newTrain.freq);
+  //This is how we access the data in firebase by setting it to a variable
+  var trainDatabase = firebase.database();
 
-  $("#trainNameInput").val("");
-  $("#destInput").val("");
-  $("#firstTrainInput").val("");
-  $("#freqInput").val("");
+  //Submit with enter key
+  $(document).bind("keydown", function(e) {
+    if (e.keyCode == 13) {
+      e.preventDefault();
 
-  return false;
-});
+      $("#submit").trigger("click");
+    }
+  });
 
-database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
+  //Grabbing the values from the inputs and setting them to the global variables
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
 
-  var trainName = childSnapshot.val().name;
-  var destination = childSnapshot.val().dest;
-  var firstTrain = childSnapshot.val().first;
-  var frequency = childSnapshot.val().freq;
+    //Stores user's
+    var TrainName = $("#trainname")
+      .val()
+      .trim();
+    var Destination = $("#destination")
+      .val()
+      .trim();
+    var StartTime = $("#traintime")
+      .val()
+      .trim();
+    var Frequency = $("#trainrate")
+      .val()
+      .trim();
 
-  console.log(trainName);
-  console.log(destination);
-  console.log(firstTrain);
-  console.log(frequency);
+    var trainData = {
+      TrainName: TrainName,
+      Destination: Destination,
+      FirstTrainTime: StartTime,
+      Frequency: Frequency
+    };
 
-  var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
-  console.log(firstTimeConverted);
+    if (TrainName && destination && StartTime && Frequency) {
+      //Pushing the user inputs to firebase
+      trainDatabase.ref().push(trainData);
+    }
 
-  var currentTime = moment();
-  console.log("CURRENT TIME:" + moment(currentTime).format("HH:mm"));
+    // Clears all of the text-boxes each time we call data from firebase
+    $("#trainname").val("");
+    $("#destination").val("");
+    $("#traintime").val("");
+    $("#trainrate").val("");
+  });
 
-  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
+  //The firebase call to go through the data when a child is added to our data
+  trainDatabase.ref().on("child_added", function(childsnapshot) {
+    //Grabs key from childsnapshot and sets it to variable
+    var key = childsnapshot.key;
 
-  var tRemainder = diffTime % frequency;
-  console.log(tRemainder);
+    //Store everything in variables from the "child" data
+    var childtrainname = childsnapshot.val().TrainName;
 
-  var tMinutesTillTrain = frequency - tRemainder;
-  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    var childdestination = childsnapshot.val().Destination;
 
-  var nextTrain = moment()
-    .add(tMinutesTillTrain, "minutes")
-    .format("hh:mm");
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    var ChildStartTime = childsnapshot.val().FirstTrainTime;
 
-  $("#trainTable > tbody").append(
-    "<tr><td>" +
-      trainName +
-      "</td><td>" +
-      destination +
-      "</td><td>" +
-      frequency +
-      "</td><td>" +
-      nextTrain +
-      "</td><td>" +
-      tMinutesTillTrain +
-      "</td></tr>"
-  );
+    var ChildFrequency = parseInt(childsnapshot.val().Frequency);
+
+    //Converting StartTime of the train to the format 'hh:mm'
+    var firstTimeConverted = moment(ChildStartTime, "hh:mm");
+
+    //Finding the difference between the First Time when the Train leaves to the current time of the user
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    //Finding the remainder of the difference
+    var Remainder = diffTime % ChildFrequency;
+
+    //Find the difference again between Frequency and Remainder and setting it to a variable
+    var MinutesTillTrain = ChildFrequency - Remainder;
+
+    //Adding the current time of the users with the 'MinutesTillTrain' and making sure it is in minutes
+    var nextTrain = moment().add(MinutesTillTrain, "minutes");
+
+    //Converting the variable 'nextTrain' time to the format 'hh:mm a'
+    var nextTrainconverted = moment(nextTrain).format("hh:mm a");
+
+    //Uploading the results to the HTML page
+    $("#traintable > tbody").append(
+      "<tr><td>" +
+        childtrainname +
+        "</td><td>" +
+        childdestination +
+        "</td><td>" +
+        ChildFrequency +
+        "</td><td>" +
+        nextTrainconverted +
+        "</td><td>" +
+        MinutesTillTrain +
+        "<button class='btn glyphicon glyphicon-trash delete' data-name='" +
+        key +
+        "' style='float: right'>" +
+        "</button>" +
+        "</td></tr>"
+    );
+  });
+
+  // Click function to delete that current row of values in the table
+  $(document).on("click", ".delete", function() {
+    //Grabs the specific key and sets it to a variable
+    var key = $(this).attr("data-name");
+
+    //Calls firebase and removes this specific child with this key
+    trainDatabase
+      .ref()
+      .child(key)
+      .remove();
+
+    location.reload();
+
+    if (navigator.userAgent.match(/Chrome|AppleWebKit/)) {
+      window.location.href = "#traintable";
+    }
+  });
 });
